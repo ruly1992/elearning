0b21fa91c14a63c4c3c8c8715acea76c363fe32f
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2015-12-03 09:31:45 +0700

    Add seeder for master user and role

---
+++ database/seeder/user/UserSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $user   = Sentinel::getUserRepository()->createModel();
+
+        Model\Activation::truncate();
+        Model\Throttle::truncate();
+
+        $user->truncate();
+
+        $users = [
+            'su'    => ['superadmin1', 'superadmin2'],
+            'adm'   => ['admin1', 'admin2'],
+            'edt'   => ['editor1', 'editor2'],
+            'ctr'   => ['contributor1', 'contributor2'],
+            'ins'   => ['instructor1', 'instructor2'],
+            'lnr'   => ['learner1', 'learner2', 'kader1', 'kader2'],
+            'pcp'   => ['principal1', 'principal2'],
+            'mdr'   => ['moderator1', 'moderator2'],
+            'ast'   => ['asisten1', 'asisten2'],
+            'pus'   => ['pustakawan1', 'pustakawan2'],
+            'ta'    => ['tenagaahli1', 'tenagaahli2'],
+        ];
+
+        foreach ($users as $role_slug => $usernames) {
+            $role = Sentinel::findRoleBySlug($role_slug);
+
+            foreach ($usernames as $username) {
+                $user = Sentinel::registerAndActivate([
+                    'email'     => $username . '@kaderdesa.id',
+                    'password'  => '123',
+                ]);
+
+                $role->users()->attach($user);
+            }
+        }            
+    }
+}
