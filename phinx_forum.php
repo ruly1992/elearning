d7d8764afd2ac20234a09aa00df58564973ab2bd
Radhian Prayogo <radhian21@gmail.com>
2015-12-04 16:31:12 +0700

    migrasi database untuk forum

---
+++ phinx_forum.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'app/bootstrap/autoload.php';
+
+$_SERVER['PHINX_MIGRATION_PATH']	= '%%PHINX_CONFIG_DIR%%/database/migrations/forum';
+$_SERVER['PHINX_DB_DATABASE']		= getenv('FORUM_DB_DATABASE') ?: 'development';
+$_SERVER['PHINX_DB_USERNAME']		= getenv('FORUM_DB_USERNAME') ?: 'root';
+$_SERVER['PHINX_DB_PASSWORD']		= getenv('FORUM_DB_PASSWORD');
+$_SERVER['PHINX_DB_PORT']			= getenv('FORUM_DB_PORT') ?: '3306';
+$_SERVER['UNIX_SOCKET']				= getenv('UNIX_SOCKET') ?: '';
+$_SERVER['PHINX_DB_PREFIX']			= getenv('FORUM_DB_PREFIX') ?: '';
+
+return include 'phinx.php';
