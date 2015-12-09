8d4db61bf40cef50452a2d8e85af53c790e85a5a
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2015-12-09 13:03:32 +0700

    Add generate menus each role user

--- admin/application/core/MY_Controller.php
+++ admin/application/core/MY_Controller.php
@@ -5,13 +5,14 @@ class MY_Controller extends CI_Controller {}
 
 class Admin extends CI_Controller
 {
-    protected $roles = array('su', 'adm', 'edt', 'ctr', 'lnr');
+    protected $roles = array('su', 'adm', 'edt', 'ctr', 'lnr', 'pus');
 
     public function __construct()
     {
         parent::__construct();
 
         $this->check();
+        $this->generateNavigator();
     }
 
     public function check()
@@ -27,9 +28,42 @@ class Admin extends CI_Controller
         }
     }
 
-    public function checkRole()
+    protected function generateNavigator()
     {
+        $role       = sentinel()->getUser()->roles->pluck('slug')->toArray();
+        $menus      = $this->getMenusByRole($role);
+        $template   = $this->load->view('template/menus', compact('menus'), TRUE);
+
+        $this->template->set('navigator', $template);
+
+        return $template;
+    }
+
+    protected function getMenusByRole($role = 'su')
+    {
+        $menus      = $this->getMenuCollect();
+
+        $filtered   = $menus->filter(function ($menu) use ($role) {
+            if (array_key_exists('roles', $menu)) {
+                if (in_array('all', $menu['roles']))
+                    return true;
+
+                return !array_diff($role, $menu['roles']);
+            } else {
+                return false;
+            }
+        });
+
+        return $filtered;
+    }
+
+    protected function getMenuCollect()
+    {
+        $this->config->load('menus');
         
+        $menus = $this->config->item('menus');
+
+        return collect($menus);
     }
 }
 
