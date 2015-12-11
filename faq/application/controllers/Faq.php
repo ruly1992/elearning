2370f40321921b31d9ad1f2669b0555530df2b5c
Radhian Prayogo <radhian21@gmail.com>
2015-12-11 10:07:42 +0700

    Update controller untuk CRUD FAQ

---
+++ faq/application/controllers/Faq.php
@@ -0,0 +1,106 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Faq extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('model_faq');
+    }
+
+    public function index(){
+    	if($this->session->flashdata('success')){
+    		$data['success'] = $this->session->flashdata('success');
+    	}elseif($this->session->flashdata('failed')){
+    		$data['failed'] = $this->session->flashdata('failed');
+    	}
+    	$data['faq'] = $this->model_faq->getFAQs();
+    	$this->load->view('dashboard',$data);
+    }
+
+    public function create(){
+    	$this->load->view('post');
+    }
+
+    public function save(){
+    	$this->form_validation->set_rules('title','Title','required');
+    	$this->form_validation->set_rules('pertanyaan','Pertanyaan','required');
+    	$this->form_validation->set_rules('jawaban','Jawaban','required');
+
+    	if($this->form_validation->run()==TRUE){
+
+    		$data = array(
+    			'title'	     => set_value('title'),
+    			'question' => set_value('pertanyaan'),
+    			'answer'	 => set_value('jawaban'),
+    			'created_at' => date('Y-m-d').' '.date('G:i:s')
+    		);
+
+    		$save = $this->model_faq->save($data);
+
+    		if($save==TRUE){
+    			$this->session->set_flashdata('success','FAQ baru berhasil ditambahkan');
+    		}else{
+    			$this->session->set_flashdata('failed','FAQ baru tidak berhasil disimpan');
+    		}
+    		redirect('faq/');
+
+    	}else{
+
+    		$this->session->set_flashdata('failed',validation_error());
+    		redirect('faq/');
+
+    	}
+    }
+
+    public function edit($id){
+        $getFAQ= $this->model_faq->getFaq($id);
+        foreach($getFAQ as $f){
+            $data = array(
+                'id'         => $f->id,
+                'title'      => $f->title,
+                'pertanyaan' => $f->question,
+                'jawaban'    => $f->answer
+            );
+        }
+        $this->load->view('edit',$data);
+    }
+
+    public function update($id){
+        $this->form_validation->set_rules('title','Title','required');
+        $this->form_validation->set_rules('pertanyaan','Pertanyaan','required');
+        $this->form_validation->set_rules('jawaban','Jawaban','required');
+
+        if($this->form_validation->run()==TRUE){
+            $data = array(
+                'title'      => set_value('title'),
+                'question'   => set_value('pertanyaan'),
+                'answer'     => set_value('jawaban'),
+                'updated_at' => date('Y-m-d').' '.date('G:i:s')
+            );
+            $update = $this->model_faq->update($id,$data);
+
+            if($update==TRUE){
+                $this->session->set_flashdata('success', 'FAQ berhasil diperbarui');
+            }else{
+                $this->session->set_flashdata('failed', 'FAQ tidak berhasil diperbarui');
+            }
+            redirect('faq/');
+        }else{
+            $this->session->set_flashdata('failed',validation_error());
+            redirect('faq/');
+        }
+    }
+
+    public function delete($id){
+    	$delete = $this->model_faq->delete($id);
+    	if($delete==TRUE){
+    		$this->session->set_flashdata('success','FAQ berhasil dihapus');
+    	}else{
+    		$this->session->set_flashdata('failed','FAQ tidak berhasil dihapus');
+    	}
+    	redirect('faq/');
+    }
+}
