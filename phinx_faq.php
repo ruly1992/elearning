864911ae2570cafe47953a506c61afef42f601a7
Radhian Prayogo <radhian21@gmail.com>
2015-12-11 10:12:11 +0700

    Update konfigurasi phinx_faq

--- phinx_faq.php
+++ phinx_faq.php
@@ -3,11 +3,11 @@ require_once 'app/bootstrap/autoload.php';
 
 $_SERVER['PHINX_MIGRATION_PATH']	= '%%PHINX_CONFIG_DIR%%/database/migrations/faq';
 $_SERVER['PHINX_SEEDER_PATH']		= '%%PHINX_CONFIG_DIR%%/database/seeder/faq';
-$_SERVER['PHINX_DB_DATABASE']		= getenv('FORUM_DB_DATABASE') ?: 'development';
-$_SERVER['PHINX_DB_USERNAME']		= getenv('FORUM_DB_USERNAME') ?: 'root';
-$_SERVER['PHINX_DB_PASSWORD']		= getenv('FORUM_DB_PASSWORD');
-$_SERVER['PHINX_DB_PORT']			= getenv('FORUM_DB_PORT') ?: '3306';
+$_SERVER['PHINX_DB_DATABASE']		= getenv('FAQ_DB_DATABASE') ?: 'development';
+$_SERVER['PHINX_DB_USERNAME']		= getenv('FAQ_DB_USERNAME') ?: 'root';
+$_SERVER['PHINX_DB_PASSWORD']		= getenv('FAQ_DB_PASSWORD');
+$_SERVER['PHINX_DB_PORT']			= getenv('FAQ_DB_PORT') ?: '3306';
 $_SERVER['UNIX_SOCKET']				= getenv('UNIX_SOCKET') ?: '';
-$_SERVER['PHINX_DB_PREFIX']			= getenv('FORUM_DB_PREFIX') ?: '';
+$_SERVER['PHINX_DB_PREFIX']			= getenv('FAQ_DB_PREFIX') ?: '';
 
 return include 'phinx.php';
