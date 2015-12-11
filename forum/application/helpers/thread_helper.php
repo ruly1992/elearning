7e1e82b2b09f89ad8a5562df977ce1da9c7df995
Radhian Prayogo <radhian21@gmail.com>
2015-12-11 13:36:05 +0700

    merapikan baris kode

--- forum/application/helpers/thread_helper.php
+++ forum/application/helpers/thread_helper.php
@@ -2,20 +2,20 @@
 
 	function countViewer($visitors, $idThread){
 		$views = 0;
-	    foreach ($visitors as $v) {
-	        if($v->thread == $idThread){
-	            $views = $views+1;
-	        }
-	    }
+		    foreach ($visitors as $v) {
+		        if($v->thread == $idThread){
+		            $views = $views+1;
+		        }
+		    }
 	    return $views;
 	}
 
 	function countComments($comments, $idThread){
 		$sum_comments = 0;
-        foreach($comments as $c){
-            if($c->reply_to == $idThread){
-                $sum_comments = $sum_comments+1;
-            }
-        }
+	        foreach($comments as $c){
+	            if($c->reply_to == $idThread){
+	                $sum_comments = $sum_comments+1;
+	            }
+	        }
         return $sum_comments;
 	}
