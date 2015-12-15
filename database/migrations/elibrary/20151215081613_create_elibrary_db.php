474c4a9050ee0d6aa3da98d03eecf1c2a49de315
Radhian Prayogo <radhian21@gmail.com>
2015-12-15 16:01:53 +0700

    tambah konfigurasi migrate database elibrary

---
+++ database/migrations/elibrary/20151215081613_create_elibrary_db.php
@@ -0,0 +1,74 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateElibraryDb extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('categories');
+        $table->addColumn('name', 'string')
+            ->addColumn('description', 'string')
+            ->addColumn('parent', 'string')
+            ->addColumn('order', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->create();
+
+        $table = $this->table('media');
+        $table->addColumn('file_name', '')
+            ->addColumn('file_type', '')
+            ->addColumn('file_size', '')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addColumn('deleted_at', 'datetime')
+            ->create();
+
+        $table = $this->table('metadata');
+        $table->addColumn('key', 'string')
+            ->addColumn('value', 'text')
+            ->addColumn('media_id', 'integer')
+            ->create();
+
+        $table = $this->table('metadata_standards');
+        $table->addColumn('name', 'string')
+            ->addColumn('description', 'string')
+            ->create();
+
+        $table = $this->table('metadata_standard_details');
+        $table->addColumn('key', 'string')
+            ->addColumn('description', 'text')
+            ->addColumn('standard_id', 'integer')
+            ->create();
+
+        $table = $this->table('settings');
+        $table->addColumn('key', 'string')
+            ->addColumn('value', 'string')
+            ->create();
+
+        $table = $this->table('tags');
+        $table->addColumn('name', 'string')
+            ->create();
+    }
+}
