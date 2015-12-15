7765361a64ed86595ca006b8f02f8cbe827dcfa2
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2015-12-15 16:18:12 +0700

    Add to view artikel terkini

--- portal/application/modules/homepage/views/index.php
+++ portal/application/modules/homepage/views/index.php
@@ -3,53 +3,34 @@
     <div class="content-articles-heading">
         <h3>Artikel Terkini</h3>
     </div>
-    <div class="content-articles-heading-view">
-        <span class="pull-right"><a href="">view all <i class="fa fa-plus-square"></i></a></span>
-    </div>
-    <div class="content-articles-main latest">
+    <div class="content-articles-main news2">
         <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 left">
-                    <?php foreach ($latest->slice(0, 5) as $artikel): ?>
-                        <div class="box-articles-latest">
-                            <div class="row">
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <div class="box-articles-latest-img">
-                                        <a href="<?php echo $artikel->link ?>"><img src="<?php echo $artikel->featured_image ?>" alt=""></a>
-                                    </div>
+            <div class="col-lg-12">
+                <?php foreach ($latest->slice(0, 5) as $artikel): ?>
+                    <div class="articles-box">
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="articles-box-img">
+                                    <a href="<?php echo $artikel->link ?>"><img src="<?php echo $artikel->featured_image ?>" alt=""></a>
                                 </div>
-                                <div class="col-md-8 col-sm-8 col-xs-12">
-                                    <div class="box-articles-latest-meta">
-                                        <p><i class="fa fa-calendar"></i> <?php echo $artikel->date->format('d F Y') ?></p>
-                                    </div>
-                                    <div class="box-articles-latest-title">
-                                        <h4><a href="<?php echo $artikel->link ?>"><?php echo $artikel->title ?></a></h4>
-                                    </div>
+                            </div>
+                            <div class="col-md-8 col-sm-8 col-xs-12">
+                                <div class="articles-box-meta">
+                                    <ul>
+                                        <li><i class="fa fa-calendar"></i> <?php echo $artikel->date->format('d F Y') ?></li>
+                                    </ul>
+                                </div>
+                                <div class="articles-box-title">
+                                    <h3><a href="<?php echo $artikel->link ?>"><?php echo $artikel->title ?></a></h3>
+                                </div>
+                                <div class="articles-box-content">
+                                    <p><?php echo $artikel->getExcerpt(250) ?></p>
                                 </div>
                             </div>
                         </div>
-                    <?php endforeach ?>                    
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 right">
-                    <?php foreach ($latest->slice(5, 5) as $artikel): ?>
-                        <div class="box-articles-latest">
-                            <div class="row">
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <div class="box-articles-latest-img">
-                                        <a href="<?php echo $artikel->link ?>"><img src="<?php echo $artikel->featured_image ?>" alt=""></a>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-sm-8 col-xs-12">
-                                    <div class="box-articles-latest-meta">
-                                        <p><i class="fa fa-calendar"></i> <?php echo $artikel->date->format('d F Y') ?></p>
-                                    </div>
-                                    <div class="box-articles-latest-title">
-                                        <h4><a href="<?php echo $artikel->link ?>"><?php echo $artikel->title ?></a></h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach ?>                    
-                </div>    
+                    </div>
+                <?php endforeach ?>                    
+            </div>
         </div>
     </div>
 </section>
