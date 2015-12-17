157b46ff49123d8766f6d7cb491ef17a6c7fb358
Radhian Prayogo <radhian21@gmail.com>
2015-12-17 12:11:51 +0700

    update model untuk menampilkan topic sesuai hak akses

--- forum/application/models/model_topic.php
+++ forum/application/models/model_topic.php
@@ -43,6 +43,12 @@ class Model_topic extends CI_Model
         return $get->result();
     }
 
+    function get_topics_from_id($id){
+        $data = array('categories.category_name','topics.*');
+        $get = $this->db->select($data)->from('topics')->join('categories','categories.id=topics.category')->where('tenaga_ahli', $id)->order_by('topics.id','desc')->get();
+        return $get->result();
+    }
+
     function get_categories(){
         $get = $this->db->get('categories');
         return $get->result();
