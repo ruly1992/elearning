a16c2110169d59e5b66db248f259dc95714090e8
ahsansandiah <sandroid.san@gmail.com>
2015-12-17 10:51:57 +0700

    modified untuk menyesuaikan PSR2

--- konsultasi/application/modules/dashboard/controllers/Dashboard.php
+++ konsultasi/application/modules/dashboard/controllers/Dashboard.php
@@ -1,25 +1,25 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
-
-	public function __construct()
-	{
-		parent::__construct();
-		
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        
         $this->load->model('konsultasi/M_konsultasi');
-	}
+    }
 
     public function index()
     {
-    	$data['categories'] = $this->M_konsultasi->getKatByUser();
-    	
+        $data['categories'] = $this->M_konsultasi->getKatByUser();
+        
         $this->template->build('kategori', $data);
     }
 
     public function kategori($kategori_id)
     {
-    	$data['konsultasi']  = $this->M_konsultasi->getListKat($kategori_id);
+        $data['konsultasi']  = $this->M_konsultasi->getListKat($kategori_id);
         $data['id_kategori'] = $kategori_id;
 
         $this->template->build('listkonsultasi', $data);
@@ -29,32 +29,26 @@ class Dashboard extends CI_Controller {
     {
         $this->form_validation->set_rules('isi', 'Isi', 'required');
 
-        if ($this->form_validation->run() == FALSE) {
-            
+        if ($this->form_validation->run() == FALSE) {            
             $detail['konsultasi']       = $this->M_konsultasi->getByIdKonsultasi($id);
             $detail['kategori']         = $this->M_konsultasi->getKatByKons($id);
             $detail['reply']            = $this->M_konsultasi->getReply($id);
 
             $this->template->build('detail', $detail);
-
         } else {
-
-            $config['upload_path'] = PATH_KONSULTASI_ATTACHMENT;
-            $config['allowed_types'] = 'gif|jpg|jpeg|png|pdf|doc|xls|xlsx|docx|zip|txt|ppt|pptx';
-            $config['max_size'] = '5000';
+            $config['upload_path']      = PATH_KONSULTASI_ATTACHMENT;
+            $config['allowed_types']    = 'gif|jpg|jpeg|png|pdf|doc|xls|xlsx|docx|zip|txt|ppt|pptx';
+            $config['max_size']         = '5000';
 
             $this->load->library('upload', $config);
 
-            if (! $this->upload->do_upload('files')) {
-                
+            if (! $this->upload->do_upload('files')) {                
                 $reply = array(
                     'isi'           => set_value('isi'),
                     'id_konsultasi' => $id,
                     'id_user'       => sentinel()->getUser()->id,
                 );
-
             } else {
-
                 $file_data = $this->upload->data();
 
                 $reply = array(
@@ -66,9 +60,8 @@ class Dashboard extends CI_Controller {
             }
             
             $id_konsultasi      = set_value('id_konsultasi');
-
-            $save             = $this->M_konsultasi->sendReply($reply, $id_konsultasi);
-            $updateKonsultasi = $this->M_konsultasi->update($id);
+            $save               = $this->M_konsultasi->sendReply($reply, $id_konsultasi);
+            $updateKonsultasi   = $this->M_konsultasi->update($id);
 
             redirect('dashboard/detail/'.$id);
         }
