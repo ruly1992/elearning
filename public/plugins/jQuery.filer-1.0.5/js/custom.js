b8b216482f28e2a5441f541905a342ff3b6ca1a9
ahsansandiah <sandroid.san@gmail.com>
2015-12-23 09:51:34 +0700

    marge

--- public/plugins/jQuery.filer-1.0.5/js/custom.js
+++ public/plugins/jQuery.filer-1.0.5/js/custom.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    
-    //Example 1
     $('#filer_input').filer({
         limit : 1,
         showThumbs: true,
@@ -60,5 +58,72 @@ $(document).ready(function() {
                 remove: '.jFiler-item-trash-action'
             }
         },
+    });
+
+    $('#filer_input_update').filer({
+    showThumbs: true,
+        templates: {
+            box: '<ul class="jFiler-items-list jFiler-items-grid"></ul>',
+            item: '<li class="jFiler-item">\
+                        <div class="jFiler-item-container">\
+                            <div class="jFiler-item-inner">\
+                                <div class="jFiler-item-thumb">\
+                                    <div class="jFiler-item-status"></div>\
+                                    <div class="jFiler-item-info">\
+                                        <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                    </div>\
+                                    {{fi-image}}\
+                                </div>\
+                                <div class="jFiler-item-assets jFiler-row">\
+                                    <ul class="list-inline pull-left"></ul>\
+                                    <ul class="list-inline pull-right">\
+                                        <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </li>',
+            itemAppend: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                            <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <li><span class="jFiler-item-others">{{fi-icon}}</span></li>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+            itemAppendToEnd: false,
+            removeConfirmation: true,
+            _selectors: {
+                list: '.jFiler-items-list',
+                item: '.jFiler-item',
+                remove: '.jFiler-item-trash-action'
+            }
+        },
+        addMore: true,
+        files: [
+            {
+                // id: $(this).attr("idKonsultasi"),
+                url: "<?php site_url(konsultasi/update) ?>",
+                name: "appended_file.jpg",
+                size: 5453,
+                type: "image/jpg",
+                file: "/path/to/file.jpg"
+            },
+        ]
     });
 });
