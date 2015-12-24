78c05a49098a8b02a74f95ae96a8a5b39352544f
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2015-12-24 07:47:55 +0700

    Tambah fitur localStorage

--- public/javascript/kelas.vue.js
+++ public/javascript/kelas.vue.js
@@ -3,42 +3,93 @@ $(document).ready(function () {
 
     var store = {
         fetch: function () {
-            return JSON.parse(localStorage.getItem(STORAGE_KEY || '[]'))
+            return JSON.parse(localStorage.getItem(STORAGE_KEY))
         },
         save: function (course) {
             localStorage.setItem(STORAGE_KEY, JSON.stringify(course));
+            console.log(JSON.stringify(course))
+        },
+        destroy: function () {
+            localStorage.removeItem(STORAGE_KEY);
         }
     }
 
-    new Vue({
+    function objChapter() {
+        this.name = '';
+        this.description = '';
+        this.quiz = [];
+    }
+
+    function objChapterQuiz() {
+        this.question = '';
+        this.answer = {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        };
+        this.correct = '';
+    }
+
+    function objCourse() {
+        this.name = '';
+        this.category = 0;
+        this.chapters = [];
+        this.exams = [];
+    }
+
+    window.app = new Vue({
         el: '#kelas-app',
         data: {
-            chapters: [
-                {
-                    id: 1,
-                    name: 'Belajar Dengan Pensil',
-                    description: 'Nantinya akan diajari dengan pensil',
-                    quiz: [
-                        {
-
-                        }
-                    ]
-                },
-                {
-                    id: 2,
-                    name: 'Belajar Dengan Bolpoin',
-                    description: 'Jangan lupa bawa bolpoin'
+            course: new objCourse,
+            input: {
+                chapterQuiz: new objChapterQuiz
+            }
+        },
+        watch: {
+            course: {
+                deep: true,
+                handler: function (val, oldVal) {
+                    store.save(val);
                 }
-            ]
+            }
         },
         methods: {
+            initData: function (storage) {
+                if (!storage) {
+                    storage = store.fetch();
+                }
+
+                $.extend(this.course, storage);
+            },
             addChapter: function () {
-                this.chapters.push({
-                    id: 0,
-                    name: '',
-                    description: ''
-                })
+                this.course.chapters.push(new objChapter);
+            },
+            addChapterQuiz: function (chapterIndex) {
+                this.input.chapterQuiz.chapterIndex = chapterIndex;
+            },
+            saveChapterQuiz: function () {
+                var index = this.input.chapterQuiz.chapterIndex;
+                var chapter = this.course.chapters[index];
+
+                chapter.quiz.push(this.input.chapterQuiz);
+
+                this.resetInput('chapterQuiz', new objChapterQuiz);
+            },
+            resetInput: function (name, data) {
+                if (name) {
+                    if (data)
+                        this.input[name] = data;
+                    else
+                        this.input[name] = [];
+                }
+            },
+            reset: function () {
+                store.destroy();
+                this.course = new objCourse;
             }
         }
     })
+
+    app.initData();
 })
