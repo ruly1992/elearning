74035d23fe19f0a9c64c2aa8134c7b1ac444f5dd
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2015-12-24 21:26:42 +0700

    Menghilangkan form_open dan merapikan spasi

--- portal/application/modules/submitarticle/views/create.php
+++ portal/application/modules/submitarticle/views/create.php
@@ -1,81 +1,78 @@
+<?php echo show_message() ?>
 
-<?php echo form_open('submitarticle'); ?>
-    <?php echo show_message() ?>
-
-    <section class="content-articles">
-        <div class="content-articles-heading">
-            <h3>Kirim Artikel</h3>
-        </div>
-    </section>
-
-    <div class="content-articles-content">
-        <fieldset class="form-group">
-            <div class="row">
-                <div class="col-md-4">
-                    <label>Foto Pengirim</label>
-                    <cropit-preview name="customavatar" :width="192" :height="192" image-empty="<?php echo asset('images/default_avatar_male.jpg') ?>">
-                        <button type="button" class="btn btn-danger" v-on:click="remove('customavatar')" slot="button-remove"><i class="fa fa-trash-o"></i></button>
-                    </cropit-preview>
-                </div>
-                <div class="col-md-6">
-                    <fieldset class="form-group">
-                        <label for="nama">Nama Lengkap</label>
-                        <?php echo form_input('nama', set_value('nama'), ['class' => 'form-control', 'id' => 'nama']) ?>
-                    </fieldset>
-                    
-                    <fieldset class="form-group">
-                        <label for="email">Email</label>
-                        <?php echo form_input('email', set_value('email'), ['class' => 'form-control', 'id' => 'email']) ?>
-                    </fieldset>
-                    
-                    <fieldset class="form-group">
-                        <label for="provinsi">Propinsi</label>
-                        <?php echo $this->wilayah->generateSelectProvinsi() ?>
-                    </fieldset>
-                    
-                    <fieldset class="form-group">
-                        <label for="kota">Kota/Kabupaten</label>
-                        <?php echo $this->wilayah->generateSelectKota() ?>
-                    </fieldset>
-
-                    <fieldset class="form-group">
-                        <label for="kecamatan">Kecamatan</label>
-                        <?php echo $this->wilayah->generateSelectKecamatan() ?>
-                   </fieldset>
-
-                    <fieldset class="form-group">
-                        <label for="desa">Desa</label>
-                        <?php echo $this->wilayah->generateSelectDesa() ?>
-                    </fieldset>
-                </div>
-            </div>   
-        </fieldset>
-        
-        <fieldset class="form-group">
-            <label for="title">Judul Artikel</label>
-            <input type="text" name="title" class="form-control" id="title">
-        </fieldset>
-        <fieldset class="form-group">
-            <textarea name="content" class="editor"></textarea>
-        </fieldset>
+<section class="content-articles">
+    <div class="content-articles-heading">
+        <h3>Kirim Artikel</h3>
     </div>
+</section>
 
-    <section class="content-articles">
-        <div class="content-articles-heading">
-            <h3>Tahap Moderasi</h3>
-        </div>
-    </section>
+<div class="content-articles-content">
+    <fieldset class="form-group">
+        <div class="row">
+            <div class="col-md-4">
+                <label>Foto Pengirim</label>
+                <cropit-preview name="customavatar" :width="192" :height="192" image-empty="<?php echo asset('images/default_avatar_male.jpg') ?>">
+                    <button type="button" class="btn btn-danger" v-on:click="remove('customavatar')" slot="button-remove"><i class="fa fa-trash-o"></i></button>
+                </cropit-preview>
+            </div>
+            <div class="col-md-6">
+                <fieldset class="form-group">
+                    <label for="nama">Nama Lengkap</label>
+                    <?php echo form_input('nama', set_value('nama'), ['class' => 'form-control', 'id' => 'nama']) ?>
+                </fieldset>
+                
+                <fieldset class="form-group">
+                    <label for="email">Email</label>
+                    <?php echo form_input('email', set_value('email'), ['class' => 'form-control', 'id' => 'email']) ?>
+                </fieldset>
+                
+                <fieldset class="form-group">
+                    <label for="provinsi">Propinsi</label>
+                    <?php echo $this->wilayah->generateSelectProvinsi() ?>
+                </fieldset>
+                
+                <fieldset class="form-group">
+                    <label for="kota">Kota/Kabupaten</label>
+                    <?php echo $this->wilayah->generateSelectKota() ?>
+                </fieldset>
 
-    <div class="content-articles-content">
-        <p>Setiap artikel yang Anda kirim akan masuk ke tahap moderasi terlebih dahulu.</p>
-        <fieldset class="form-group">
-            <button id="asd" type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Kirim Artikel</button>
-        </fieldset>
+                <fieldset class="form-group">
+                    <label for="kecamatan">Kecamatan</label>
+                    <?php echo $this->wilayah->generateSelectKecamatan() ?>
+               </fieldset>
+
+                <fieldset class="form-group">
+                    <label for="desa">Desa</label>
+                    <?php echo $this->wilayah->generateSelectDesa() ?>
+                </fieldset>
+            </div>
+        </div>   
+    </fieldset>
+    
+    <fieldset class="form-group">
+        <label for="title">Judul Artikel</label>
+        <input type="text" name="title" class="form-control" id="title">
+    </fieldset>
+    <fieldset class="form-group">
+        <textarea name="content" class="editor"></textarea>
+    </fieldset>
+</div>
+
+<section class="content-articles">
+    <div class="content-articles-heading">
+        <h3>Tahap Moderasi</h3>
     </div>
+</section>
 
-    <cropit-result name="featured"></cropit-result>
-    <cropit-result name="customavatar"></cropit-result>
-<?php echo form_close(); ?>
+<div class="content-articles-content">
+    <p>Setiap artikel yang Anda kirim akan masuk ke tahap moderasi terlebih dahulu.</p>
+    <fieldset class="form-group">
+        <button id="asd" type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Kirim Artikel</button>
+    </fieldset>
+</div>
+
+<cropit-result name="featured"></cropit-result>
+<cropit-result name="customavatar"></cropit-result>
 
 <?php custom_script() ?>
     <script src="<?php echo asset('node_modules/jquery-chained/jquery.chained.remote.js') ?>"></script>
