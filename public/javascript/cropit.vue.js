d1d813fc8a938f1e7b57220e5955a446068846e0
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2015-12-24 20:36:39 +0700

    Add component VueJS for cropitjs

---
+++ public/javascript/cropit.vue.js
@@ -0,0 +1,153 @@
+$(document).ready(function () {
+    Vue.component('cropit-preview', {
+        template: '#cropit-preview',
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            width: {
+                default: 0
+            },
+            height: {
+                default: 0
+            },
+            imageEmpty: {
+                type: String,
+                default: '/public/images/portal/img-carousel-default.jpg'
+            },
+            imageSrc: {
+                type: String,
+                default: function () {
+                    return this.imageEmpty;
+                }
+            },
+            action: {
+                default: 'none'
+            }
+        },
+        watch: {
+            'action': {
+                handler: function (action, lastAction) {
+                    this.$parent.$broadcast('actioning', {
+                        name: this.name,
+                        action: action
+                    });
+                }
+            }
+        },
+        methods: {
+            setImage: function (imageSrc) {
+                this.imageSrc = imageSrc;
+                this.action = 'upload';
+            },
+            remove: function () {
+                this.imageSrc = this.imageEmpty;
+                this.action = 'remove';
+
+                this.$parent.$broadcast('removed', this.name);
+            }
+        },
+        events: {
+            'cropped': function (imageEvent) {
+                if (this.name == imageEvent.name)
+                    this.setImage(imageEvent.src)
+            },
+            'removing': function (name) {
+                if (this.name == name)
+                    this.remove();
+            }
+        }
+    })
+
+    Vue.component('cropit-result', {
+        template: '#cropit-result',
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            imageSrc: {
+                default: ''
+            },
+            action: {
+                default: 'none'
+            }
+        },
+        events: {
+            'cropped': function (imageEvent) {
+                if (this.name == imageEvent.name)
+                    this.imageSrc = imageEvent.src;
+            },
+            'actioning': function (actionEvent) {
+                if (this.name == actionEvent.name)
+                    this.action = actionEvent.action;
+            },
+            'removed': function (imageName) {
+                if (this.name == imageName)
+                    this.imageSrc = '';
+            }
+        }
+    })
+
+    Vue.component('cropit-cropper', {
+        template: '#cropit-cropper',
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            width: {
+                type: Number,
+                default: 632
+            },
+            height: {
+                type: Number,
+                default: 302
+            },
+            imageSrc: {
+                type: String,
+                default: ''
+            }
+        },
+        methods: {
+            setImagePreview: function () {
+                var imageEvent = {
+                    name: this.name,
+                    src: this.imageSrc
+                }
+
+                this.$parent.$broadcast('cropped', imageEvent);
+            }
+        },
+        events: {
+            'cropping': function (name) {
+                if (this.name == name)
+                    this.setImagePreview()
+            }
+        },
+        ready: function () {
+            var that = this;
+            var $cropitEl = $(this.$el);
+            
+            $cropitEl.cropit({
+                onOffsetChange: function (offset) {
+                    var imageData = $cropitEl.cropit('export');
+                    that.imageSrc = imageData;
+                }
+            })
+        }
+    })
+
+    new Vue({
+        el: '#app-cropit',
+        methods: {
+            save: function (name) {
+                this.$broadcast('cropping', name)
+            },
+            remove: function (name) {
+                this.$broadcast('removing', name)
+            }
+        }
+    })
+})
