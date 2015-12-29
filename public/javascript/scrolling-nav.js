b43b58c9821ed1576eebf7adceb968826ed35c40
fajarekos <fajarekosusilo10@gmail.com>
2015-12-29 12:50:44 +0700

    update navbar

--- public/javascript/scrolling-nav.js
+++ public/javascript/scrolling-nav.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 	$(window).scroll(function() {
 	    if ($("#navbar-main").offset().top > 250) {
-	        $("#navbar-main").addClass("top-nav-collapse border logo-scroll");
+	        $("#navbar-main").addClass("top-nav-collapse border navbar-brand");
 	        $("#navbar-main").find('.navbar-brand').show();
 	        $("#navbar-main").find('.header-top-right').show();
 	    } else {
-	        $("#navbar-main").removeClass("top-nav-collapse border logo-scroll");
+	        $("#navbar-main").removeClass("top-nav-collapse border navbar-brand");
 	        $("#navbar-main").find('.navbar-brand').hide();
 	        $("#navbar-main").find('.header-top-right').hide();
 	    }
