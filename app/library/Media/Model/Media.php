dd15021648a7b81f0cb7b41e9f460c9757a2575d
Radhian Prayogo <radhian21@gmail.com>
2015-12-30 10:05:35 +0700

    update fungsi getFilepath

--- app/library/Media/Model/Media.php
+++ app/library/Media/Model/Media.php
@@ -29,7 +29,8 @@ class Media extends Model
 
     public function getFilepath()
     {
-        return FCPATH . '/assets/upload/media/' . $this->category->name . '/' . $this->file_name;
+        // return FCPATH . '/assets/upload/media/' . $this->category->name . '/' . $this->file_name;
+        return attachment('elibrary/media/' . $this->category->name . '/' . $this->file_name);
     }
 
     public function resolveVisitorUnique($user, $mediaID)
