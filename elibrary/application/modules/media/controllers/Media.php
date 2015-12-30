838dc9a01b17472f4447025998ef6d91a86e4358
Radhian Prayogo <radhian21@gmail.com>
2015-12-30 10:10:18 +0700

    update controller untuk menampilkan alert setelah proses

--- elibrary/application/modules/media/controllers/Media.php
+++ elibrary/application/modules/media/controllers/Media.php
@@ -17,6 +17,9 @@ class Media extends Admin
 
     public function index()
     {
+        if($this->session->flashdata('success')){
+            $data['success'] = $this->session->flashdata('success');
+        }
         $category   = $this->medialib->getCategory();
         $categories = $category->with(['media' => function ($query) {
             $query->userId(sentinel()->getUser()->id)->withDrafts();
@@ -44,6 +47,9 @@ class Media extends Admin
 
     public function upload()
     {
+        if($this->session->flashdata('failed')){
+            $data['failed']    =   $this->session->flashdata('failed');
+        }
         $category   = $this->medialib->getCategory();
         $categories = $category->with(['media' => function ($query) {
             $query->userId(sentinel()->getUser()->id)->withDrafts();
@@ -111,7 +117,7 @@ class Media extends Admin
         if($counter == ($count-1)){
             $this->fillMeta($dataFiles);
         }else{
-            $this->session->flashdata('failed', 'Media gagal diunggah');
+            $this->session->set_flashdata('failed', 'Media gagal diunggah');
             redirect('media/upload');
         }
 
@@ -196,9 +202,11 @@ class Media extends Admin
         }
 
         if($simpan == true){
-            redirect(site_url());
+            $this->session->set_flashdata('success', 'File media berhasil ditambahkan.');
+            redirect('/media');
         }else{
-            echo $simpan;
+            $this->session->set_flashdata('failed', $simpan);
+            redirect('media/upload');
         }
 
     }
