7d05866e076e5feb50e76258ffc65710eb2766cf
Radhian Prayogo <radhian21@gmail.com>
2015-12-30 13:25:11 +0700

    update view upload agar input media dan nama file harus diisi

--- elibrary/application/modules/media/views/upload.php
+++ elibrary/application/modules/media/views/upload.php
@@ -15,7 +15,7 @@
         }
     ?>
     <div class="title">
-        <?php echo form_open_multipart('media/submit'); ?>
+        <?php echo form_open_multipart('media/submit', array('id'=>'formMedia')); ?>
             <div class="form-group">
                 <label>Kategori</label>
                 <select class="form-control" name="kategori">
@@ -29,11 +29,10 @@
             <div class="form-group">
                 <input type="file" name="filemedia[]" id="filer_input_media" multiple="multiple">
             </div>
-            <button class="btn btn-danger" id="extrabutton">Start</button>
+            <button type="button" onclick="checkInput(); return false;" class="btn btn-danger" id="extrabutton">Start</button>
         <?php echo form_close(); ?>
     </div>
     <div class="description-meta">
-        <!-- <div id="fileuploader" data-url="<?php echo site_url('media/submit/') ?>" data-category-id="<?php //echo $category->id ?>">Upload</div> -->
     
     </div>
 </div>
@@ -51,4 +50,19 @@
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script type="text/javascript" src="<?php echo asset('/plugins/jQuery.filer-1.0.5/js/jquery.filer.min.js?v=1.0.5') ?>"></script>
     <script type="text/javascript" src="<?php echo asset('/javascript/jquery.filer.custom.js') ?>"></script>
+    <script type="text/javascript">
+        function checkInput(){
+            if(document.getElementById('filer_input_media').value == ''){  
+              alert('Anda harus memilih file untuk diunggah terlebih dahulu!');  
+              document.getElementById('filer_input_media').focus();  
+              return false;  
+            }
+            if(document.getElementById('fileName').value == ''){  
+              alert('Nama file harus diisi terlebih dahulu!');  
+              document.getElementById('fileName').focus();  
+              return false;  
+            }
+            document.getElementById('formMedia').submit();
+        }
+    </script>
 <?php endcustom_script() ?>
