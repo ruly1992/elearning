f2c3410ab18de2d425ba2b145506b23c71b332d3
Radhian Prayogo <radhian21@gmail.com>
2016-01-04 15:16:59 +0700

    update model thread untuk fungsi menyimpan dan menampilkan close/private thread

--- forum/application/models/model_thread.php
+++ forum/application/models/model_thread.php
@@ -5,7 +5,7 @@ class Model_thread extends CI_Model
     function save_thread($data)
     {
         $this->db->insert('threads',$data);
-        if($this->db->affected_rows() == '1'){
+        if($this->db->affected_rows() == 1){
           return TRUE;
         } else {
           return FALSE;
@@ -15,7 +15,7 @@ class Model_thread extends CI_Model
     function delete_thread($id)
     {
         $delete = $this->db->delete('threads', array('id'=>$id));
-        if($this->db->affected_rows() == '1'){
+        if($this->db->affected_rows() == 1){
             return TRUE;
         }else{
             return FALSE;
@@ -25,7 +25,7 @@ class Model_thread extends CI_Model
     function delete_thread_by_topic($idTopic)
     {
         $delete = $this->db->delete('threads', array('topic'=>$idTopic));
-        if($this->db->affected_rows() == '1'){
+        if($this->db->affected_rows() >= 1){
             return TRUE;
         }else{
             return FALSE;
@@ -37,7 +37,7 @@ class Model_thread extends CI_Model
         $this->db->where('id',$id);
         $update = $this->db->update('threads', $data); 
         
-        if($this->db->affected_rows() == '1'){
+        if($this->db->affected_rows() == 1){
             return TRUE;
         }
         return FALSE;
@@ -157,7 +157,18 @@ class Model_thread extends CI_Model
     {
         $this->db->where('reply_to', $id);
         $delete = $this->db->delete('threads');
-        if($this->db->affected_rows() == '1'){
+        if($this->db->affected_rows() >= 1){
+            return TRUE;
+        }else{
+            return FALSE;
+        }
+    }
+
+    function delete_thread_members($id)
+    {
+        $this->db->where('thread_id', $id);
+        $delete = $this->db->delete('thread_members');
+        if($this->db->affected_rows() >= 1){
             return TRUE;
         }else{
             return FALSE;
@@ -175,14 +186,53 @@ class Model_thread extends CI_Model
         return $get->result();
     }
 
+    function get_topics_by_user($id){
+        $get = $this->db->select('*')
+                    ->from('topics')
+                    ->where('tenaga_ahli', $id)
+                    ->get();
+        if($this->db->affected_rows() >= 1){
+            return $get->result();
+        }else{
+            return array();
+        }
+    }
+
     function approve_thread($data, $id)
     {   
         $this->db->where('id', $id);
         $update = $this->db->update('threads', $data);
-        if($this->db->affected_rows() == '1'){
+        if($this->db->affected_rows() == 1){
             return TRUE;
         }else{
             return FALSE;
+        }
+    }
+
+    function get_thread_by_all($where){
+        $get = $this->db->get_where('threads', $where);
+        if($this->db->affected_rows() >= 1){
+            return $get->result();
+        }else{
+            return array();
+        }
+    }
+
+    function save_thread_member($member){
+        $save   = $this->db->insert('thread_members', $member);
+        if($this->db->affected_rows() == 1){
+            return TRUE;
+        }else{
+            return FALSE;
+        }
+    }
+
+    function get_thread_members(){
+        $get = $this->db->get('thread_members');
+        if($this->db->affected_rows() >= 1){
+            return $get->result();
+        }else{
+            return array();
         }
     }
 
