bf0139b83a24b1da4e6f6aa86bd56e45d309272c
Singgih Saputro <singgihsap@gmail.com>
2016-01-09 16:07:01 +0700

    moles avatar user administrator

--- admin/application/views/template/sidebar.php
+++ admin/application/views/template/sidebar.php
@@ -2,15 +2,19 @@
 <div class="sidebar">
 
     <div class="sidebar-collapse">
-
-        <div class="sidebar-header">
-
-            <img src="<?php echo auth()->getUser()->avatar ?>">
-
-            <h2><?php echo auth()->getUser()->full_name ?></h2>
-            <h3><?php echo auth()->getUser()->email ?></h3>
-
-        </div>
+        <center>
+            <div class="sidebar-header">
+                <div class="admin-image">
+                        <img src="<?php echo auth()->getUser()->avatar ?>">
+                </div>
+                <div class="admin-info">
+                    <center>
+                        <h2><?php echo auth()->getUser()->full_name ?></h2>
+                        <h3><?php echo auth()->getUser()->email ?></h3>
+                    </center>
+                </div>
+            </div>
+        </center>
 
         <div class="sidebar-menu">
             <?php echo $navigator ?>
