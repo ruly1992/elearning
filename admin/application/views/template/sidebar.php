0d5c052680b73fc81253e4f4479fdc038355204a
Singgih Saputro <singgihsap@gmail.com>
2016-01-11 13:56:45 +0700

    dropdown profile

--- admin/application/views/template/sidebar.php
+++ admin/application/views/template/sidebar.php
@@ -9,7 +9,15 @@
                 </div>
                 <div class="admin-info">
                     <center>
-                        <h2><?php echo auth()->getUser()->full_name ?></h2>
+                        <h2><?php echo auth()->getUser()->full_name ?> 
+                            <a href="#" aria-expanded="false" type="button" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-chevron-down"></i></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="<?php echo site_url('profile') ?>"><i class="fa fa-user"></i> Profile</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?php echo logout_url() ?>"><i class="fa fa-lock"></i> Logout</a></li>
+                            </ul>
+                        </h2>
+
                         <h3><?php echo auth()->getUser()->email ?></h3>
                     </center>
                 </div>
@@ -21,7 +29,7 @@
         </div>                  
     </div>
     <div class="sidebar-footer">
-        
+
     </div>  
 </div>
 <!-- end: Main Menu -->
