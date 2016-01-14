dbcf7ae2269d41e91e6658141e3e8c5fc2decb93
ahsansandiah <sandroid.san@gmail.com>
2016-01-14 10:13:46 +0700

    perbaikan filer media dan nambah filer_konsultasi_reply

--- public/javascript/jquery.filer.custom.js
+++ public/javascript/jquery.filer.custom.js
@@ -201,7 +201,7 @@ $(document).ready(function() {
         maxSize: 20,
         extensions: ['jpg', 'jpeg', 'png', 
                     'gif', 'pdf', 'rar', 'zip', 
-                    'xlxs', 'docx', 'doc', 'xlx', 
+                    'xlsx', 'docx', 'doc', 'xls', 
                     'ppt', 'pptx', '3gp', 'mp4', 'mpeg', 
                     'mkv', 'flv', 'mp3', 'txt'],
         addMore: true,
@@ -391,6 +391,69 @@ $(document).ready(function() {
             }
         },
     });
+
+    $('#filer_konsultasi_reply').filer({
+        limit : 1,
+        maxSize: 10,
+        showThumbs: true,
+        templates: {
+            box: '<ul class="jFiler-items-list jFiler-items-grid"></ul>',
+            item: '<li class="jFiler-item">\
+                        <div class="jFiler-item-container">\
+                            <div class="jFiler-item-inner">\
+                                <div class="jFiler-item-thumb">\
+                                    <div class="jFiler-item-status"></div>\
+                                    <div class="jFiler-item-info">\
+                                        <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                    </div>\
+                                    {{fi-image}}\
+                                </div>\
+                                <div class="jFiler-item-assets jFiler-row">\
+                                    <ul class="list-inline pull-left">\
+                                        <li>{{fi-progressBar}}</li>\
+                                    </ul>\
+                                    <ul class="list-inline pull-right">\
+                                        <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </li>',
+            itemAppend: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                            <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <li><span class="jFiler-item-others">{{fi-icon}}</span></li>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+            progressBar: '<div class="bar"></div>',
+            itemAppendToEnd: false,
+            removeConfirmation: true,
+            _selectors: {
+                list: '.jFiler-items-list',
+                item: '.jFiler-item',
+                progressBar: '.bar',
+                remove: '.jFiler-item-trash-action'
+            }
+        },
+    });
+
     $('#filer_input_img').filer({
         limit : 1,
         maxSize: 10,
