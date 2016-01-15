fbbf06f3c5c076bf8a0a96b57d2644ea4c0f822a
fajarekos <fajarekosusilo10@gmail.com>
2016-01-15 15:13:24 +0700

    update tampilan pada create konsultasi

--- konsultasi/application/modules/konsultasi/views/create.php
+++ konsultasi/application/modules/konsultasi/views/create.php
@@ -15,7 +15,7 @@
                 <form method="POST" action="<?php echo site_url('konsultasi/create') ?>" enctype="multipart/form-data">
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-md-8 col-sm-8 col-xs-12">
+                            <div class="col-md-12 col-sm-12 col-xs-12">
                                 <label for="">Subjek</label>
                                 <?php echo form_input('subjek', set_value('subjek'), array('class' => 'form-control input-lg', 'placeholder' => 'Masukkan subjek Konsultasi')); ?>
                             </div>
@@ -23,7 +23,7 @@
                     </div>
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-md-4 col-sm-4 col-xs-12">
+                            <div class="col-md-6 col-sm-6 col-xs-12">
                                 <label for="">Kategori</label><br>
                                 <select class="form-control" name="id_konsultasi_kategori">
                                     <?php 
@@ -33,11 +33,7 @@
                                     <?php }?>
                                 </select>
                             </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4 col-xs-12">
+                            <div class="col-md-6 col-sm-6 col-xs-12">
                                 <label for="">Prioritas</label><br>
                                 <select class="form-control" name="prioritas">
                                     <option value="High" selected>High</option>
@@ -49,7 +45,7 @@
                     </div>
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-md-8 col-sm-8 col-xs-12">
+                            <div class="col-md-12 col-sm-12 col-xs-12">
                                 <label for="">Pesan Anda</label>
                                 <?php echo form_textarea('pesan', set_value('pesan', '', FALSE), array('class' => 'editor')); ?>
                             </div>
