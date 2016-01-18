ef30630eb20bb22e17cbaeee3fd8504b1ceaa1e5
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-01-18 15:54:22 +0700

    update kelas online with validate

--- public/javascript/kelas.wizard.js
+++ public/javascript/kelas.wizard.js
@@ -1,8 +1,43 @@
 $(document).ready(function () {
+    var form = $("#wizard-create-form");
+
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) {
+            element.before(error);
+        }
+    })
+
     $("#wizard").steps({
         headerTag: "h2",
         bodyTag: "section",
         transitionEffect: "slideLeft",
+        onStepChanging: function (event, currentIndex, newIndex) {
+            if (currentIndex === 0) {
+                return form.valid();
+            } else if (currentIndex === 2) {
+                var length = window.app_kelas_online.course.chapters.length;
+
+                if (length === 0) {
+                    alert('Anda belum memasukkan chapter.')
+
+                    return false;
+                } else {
+                    return true;
+                }
+            } else if (currentIndex === 3) {
+                var length = window.app_kelas_online.course.exams.length;
+
+                if (length === 0) {
+                    alert('Anda belum memasukkan exam.')
+
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            return true;
+        },
         onFinished: function (event, currentIndex) {
             var form = $('#form-course-result')
             
