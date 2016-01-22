d991042ee479c3944b038652de8d76d9422b19db
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-01-22 17:35:11 +0700

    index article draft

---
+++ portal/application/modules/dashboard/views/index_draft.php
@@ -0,0 +1,69 @@
+
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+       <section class="content-articles">
+            <div class="content-articles-heading">
+                <h3>Dashboard Post Articles</h3>
+            </div>
+        </section> 
+        <div class="container content-submit">
+            <div class="widget">
+                <div class="widget-heading">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                             <a class="nav-link active" id="#" data-toggle="tab" href="#article-submit" role="tab" aria-controls="article-approve" aria-expanded="true">Artikel Submit</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="widget-content">
+                    <div class="tab-content" id="myTabTableContent">
+                        <div role="tabpanel" class="tab-pane fade active in" id="article-submit" aria-expanded="true">
+                            <table id="post-articles" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Judul</th>
+                                        <th>Status</th>
+                                        <th>Waktu Terbit</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($artikel as $article): ?>
+                                        <tr>
+                                            <td><?php echo $article->id ?></td>
+                                            <td><?php echo $article->title ?></td>
+                                            <td><?php echo $article->getStatusLabel() ?></td>
+                                            <td><?php echo $article->date->format('d F Y H:i') ?></td>
+                                            <td>
+                                                <a href="#" class="btn btn-primary btn-konsul">Update</a>
+                                                <a href="#" class="btn btn-danger btn-konsul">Delete</a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php custom_stylesheet() ?>
+
+    <link rel="stylesheet" href="<?php echo asset('node_modules/datatables/media/css/jquery.dataTables.min.css') ?>">
+
+<?php endcustom_stylesheet() ?>
+
+<?php custom_script() ?>
+ <!--jQuery-->
+
+<script type="text/javascript" src="<?php echo asset('/plugins/datatables/js/jquery.dataTables.min.js') ?>"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#post-articles').dataTable();
+    });
+</script>
+<?php endcustom_script() ?>
