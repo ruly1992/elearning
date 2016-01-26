9164901cc7c60ad72805414723bc266df5409422
fajarekos <fajarekosusilo10@gmail.com>
2016-01-26 10:45:25 +0700

    update tampilan untuk dashboard

--- portal/application/modules/dashboard/views/index.php
+++ portal/application/modules/dashboard/views/index.php
@@ -134,6 +134,17 @@
                                             <h3>Recent activity Elibrary</h3>
                                         </div>
                                         <div class="recent-elibrary-content">
+                                            <div class="list-elibrary">
+                                                <h4>Library terpopuler</h4>
+                                                <ul>
+                                                    <li><a href="#">Hijab Treveler</a>
+                                                        <small><i class="fa fa-calender"></i> 26/01/2016 - <i class="fa fa-file-video-o"></i> video</small>
+                                                    </li>
+                                                    <li><a href="#">Hijab Treveler</a>
+                                                        <small><i class="fa fa-calender"></i> 26/01/2016 - <i class="fa fa-file-video-o"></i> video</small>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                             <div class="elibrary-comment">
                                                 <h4>Your Media Files</h4>
                                                 <ul>
@@ -284,146 +295,54 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <section class="content-articles">
             <div class="content-articles-heading">
-                <h3>Dashboard Submit</h3>
+                <h3>Submite Artikel</h3>
             </div>
         </section>
         <div class="container content-submit">
             <div class="widget">
-                <div class="widget-heading">
-                    <ul class="nav nav-tabs" id="myTabSubmit" role="tablist">
-                        <li class="nav-item">
-                             <a class="nav-link active" data-toggle="tab" href="#submit-article" role="tab" aria-controls="article-post" aria-expanded="true"><label for="" class="title">Artikel</label></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#submit-elibrary" role="tab" aria-controls="library-post" aria-expanded="false"><label for="" class="title">Elibrary</label></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#submit-konsultasi" role="tab" aria-controls="konsultasis-post" aria-expanded="false"><label for="" class="title">Konsultasi</label></a>
-                        </li>
-                    </ul>
-                </div>
                 <div class="row">
-                    <div class="widget-content">
-                        <div class="tab-content" id="myTabSubmitContent">
-                            <div role="tabpanel" class="tab-pane fade active in" id="submit-article" aria-labelledby="article-post" aria-expanded="true">
-                                <?php echo form_open('dashboard/sendArticle'); ?>
-                                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                                        <fieldset class="form-group">
-                                            <label for="title">Judul Artikel</label>
-                                            <input name="title" type="text" class="form-control" id="title" placeholder="">
-                                            <small class="text-muted">Masukkan judul artikel disini</small>
-                                        </fieldset>
-                                        <fieldset class="form-group">
-                                            <textarea name="content" class="editor-simple"></textarea>
-                                        </fieldset>
-                                        <fieldset class="form-group hidden-sm-up">
-                                             <input type="file" name="filemedia" id="filer_input_img">
-                                        </fieldset>
-                                        <fieldset class="form-group hidden-sm-up">
-                                            <label for="">Keterangan gambar</label>
-                                            <input type="text" class="form-control" name="caption-img">
-                                        </fieldset>
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </div>
-                                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                                        <!-- begin: category -->
-                                        <div class="widget">
-                                            <div class="widget-sidebar-heading">
-                                                <h3>Category</h3>
-                                            </div>
-                                            <div class="widget-sidebar-content">
-                                                <?php echo $categories_checkbox ?>
-                                            </div>
-                                        </div>
-                                        <!-- end: category -->
-                                        <!-- begin: image preview -->
-                                        <div class="widget hidden-lg-down">
-                                            <div class="widget-sidebar-heading">
-                                                <h3>Gambar Fitur</h3>
-                                            </div>
-                                            <div class="widget-sidebar-content">
-                                                <cropit-preview name="featured" :show-description="true"></cropit-preview>
-                                                <cropit-result name="featured"></cropit-result>
-                                            </div>
-                                        </div>
-                                    </div>
-                                <?php echo form_close(); ?>
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="submit-elibrary" aria-labelledby="" aria-expanded="false">
-                                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                                    <?php echo form_open_multipart('elibrary/media/submit', array('id'=>'formMedia')); ?>
-                                        <fieldset class="form-group">
-                                            <label>Kategori</label>
-                                            <select class="form-control" name="kategori">
-                                                <?php 
-                                                    foreach($categories AS $cat){
-                                                        echo '<option value="'.$cat->id.'">'.$cat->name.'</option>';
-                                                    }
-                                                ?>
-                                            </select>
-                                        </fieldset>
-                                        <fieldset class="form-group">
-                                           <p class="label label-info">Maximum Files 20MB</p>
-                                            <input type="file" name="filemedia[]" id="filer_input_media" multiple="multiple">
-                                        </fieldset>
-                                        <button type="submit" onclick="checkInput(); return false;" class="btn btn-primary">Submit</button>
-                                    <?php echo form_close(); ?>
+                    <?php echo form_open('dashboard/sendArticle'); ?>
+                        <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                            <fieldset class="form-group">
+                                <label for="title">Judul Artikel</label>
+                                <input name="title" type="text" class="form-control" id="title" placeholder="">
+                                <small class="text-muted">Masukkan judul artikel disini</small>
+                            </fieldset>
+                            <fieldset class="form-group">
+                                <textarea name="content" class="editor-simple"></textarea>
+                            </fieldset>
+                            <fieldset class="form-group hidden-sm-up">
+                                 <input type="file" name="filemedia" id="filer_input_img">
+                            </fieldset>
+                            <fieldset class="form-group hidden-sm-up">
+                                <label for="">Keterangan gambar</label>
+                                <input type="text" class="form-control" name="caption-img">
+                            </fieldset>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <!-- begin: category -->
+                            <div class="widget">
+                                <div class="widget-sidebar-heading">
+                                    <h3>Category</h3>
+                                </div>
+                                <div class="widget-sidebar-content">
+                                    <?php echo $categories_checkbox ?>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade" id="submit-konsultasi" aria-labelledby="" aria-expanded="false">
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">   
-                                    <form method="POST" action="<?php echo site_url('konsultasi/konsultasi/create') ?>" enctype="multipart/form-data">
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                                <label for="">Subjek</label>
-                                                <?php echo form_input('subjek', set_value('subjek'), array('class' => 'form-control input-lg', 'placeholder' => 'Masukkan subjek Konsultasi')); ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                                    <label for="">Kategori</label><br>
-                                                    <select class="form-control" name="id_konsultasi_kategori">
-                                                        <?php 
-                                                            foreach ($konsultasiCat as $row) {
-                                                        ?>
-                                                            <option value="<?php echo $row->id;?>"><?php echo $row->name;?></option>
-                                                        <?php }?>
-                                                    </select>
-                                                </div>
-                                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                                    <label for="">Prioritas</label><br>
-                                                    <select class="form-control" name="prioritas">
-                                                        <option value="High" selected>High</option>
-                                                        <option value="Medium">Medium</option>
-                                                        <option value="Low">Low</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                                    <label for="">Pesan Anda</label>
-                                                    <?php echo form_textarea('pesan', set_value('pesan', '', FALSE), array('class' => 'editor')); ?>
-                                                </div> 
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="">Attachments</label>
-                                            <input type="file" name="files" id="filer_konsultasi">
-                                            <small>(Allowed File Extensions: .jpg, .gif, .jpeg, .png, .pdf, .zip, .doc, .xls, .xlsx, .docx, .txt) </small>
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-primary">Submit</button>
-                                            </div>
-                                        </div>
-                                    </form>
+                            <!-- end: category -->
+                            <!-- begin: image preview -->
+                            <div class="widget hidden-lg-down">
+                                <div class="widget-sidebar-heading">
+                                    <h3>Gambar Fitur</h3>
+                                </div>
+                                <div class="widget-sidebar-content">
+                                    <cropit-preview name="featured" :show-description="true"></cropit-preview>
+                                    <cropit-result name="featured"></cropit-result>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    <?php echo form_close(); ?>
                 </div>
             </div>
         </div>
