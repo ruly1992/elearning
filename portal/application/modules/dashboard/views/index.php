e00535163a4af5b74954b4c7330e3e5167af7884
Singgih Saputro <singgihsap@gmail.com>
2016-01-26 15:25:46 +0700

    update dashboard portal

--- portal/application/modules/dashboard/views/index.php
+++ portal/application/modules/dashboard/views/index.php
@@ -139,13 +139,10 @@
                                                 <ul>
                                                     <?php  
                                                         if(!empty($recentMedia)){
-<<<<<<< HEAD
                                                             foreach(array_reverse($recentMedia) as $media){
                                                                 echo '<li>'.anchor($media->link, $media->title).'</li>';
-=======
                                                             foreach($recentMedia as $media){
                                                                 echo '<li class="bg-success">'.anchor($media->link, $media->title).'</li>';
->>>>>>> 4d345f5d8f364853fd28ac2d33543520a692d62f
                                                             }
                                                         }else{
                                                             echo '<li class="bg-warning">Anda belum mengunggah library</li>';
